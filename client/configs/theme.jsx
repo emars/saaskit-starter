128ab06db6624d018c56483a619354c3761b59fe
Evan Marsland <ERmarsland@gmail.com>
2016-02-23 18:08:55 -0500

    initial commit

---
+++ client/configs/theme.jsx
@@ -0,0 +1,6 @@
+// import Layout from '/client/modules/_home/components/layoutJustContent.jsx';
+import Layout from '/client/modules/_theme/components/layout.jsx';
+import Dropdown from '/client/modules/_theme/components/dropdown.jsx';
+import Pageheader from '/client/modules/_theme/components/pageheader.jsx';
+import Pagetitle from '/client/modules/_theme/components/pagetitle.jsx';
+export {Layout, Dropdown,Pageheader,Pagetitle};
